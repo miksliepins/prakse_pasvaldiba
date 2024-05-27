54876b0dbdafb72d5bb8b7e1714d99ac3ad883d5
Markuss Miks Liepiņš <miksliepins98@gmail.com>
2024-05-27 12:34:28 +0300

    pievienots uzdevums6

---
+++ uzdevums6/interneta_veikals.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2024 at 01:23 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `interneta_veikals`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_state`
+--
+
+CREATE TABLE `order_state` (
+  `order_state_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `products_id` int(11) NOT NULL,
+  `products_name` varchar(255) DEFAULT NULL,
+  `products_cena` decimal(10,2) DEFAULT NULL,
+  `products_bilde` varchar(255) DEFAULT NULL,
+  `products_apraskts` text DEFAULT NULL,
+  `products_count` int(11) DEFAULT NULL,
+  `product_category_product_category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_category`
+--
+
+CREATE TABLE `product_category` (
+  `product_category_id` int(11) NOT NULL,
+  `name_category` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_cart`
+--
+
+CREATE TABLE `user_cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_cart_cart_id` int(11) DEFAULT NULL,
+  `order_state` varchar(100) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `userinfo_id` int(11) NOT NULL,
+  `user_info_name` varchar(100) DEFAULT NULL,
+  `user_info_surname` varchar(100) DEFAULT NULL,
+  `user_info_email` varchar(100) DEFAULT NULL,
+  `user_info_adrese` varchar(100) DEFAULT NULL,
+  `user_orderTime` datetime DEFAULT NULL,
+  `user_summa` decimal(10,2) DEFAULT NULL,
+  `user_preces_nummurs` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login`
+--
+
+CREATE TABLE `user_login` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(45) NOT NULL,
+  `user_surname` varchar(45) NOT NULL,
+  `user_email` varchar(45) NOT NULL,
+  `user_password` varchar(45) NOT NULL,
+  `user_reg_datums` datetime DEFAULT NULL,
+  `user_ped_registrs` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `order_state`
+--
+ALTER TABLE `order_state`
+  ADD PRIMARY KEY (`order_state_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`products_id`),
+  ADD KEY `product_category_product_category_id` (`product_category_product_category_id`);
+
+--
+-- Indexes for table `product_category`
+--
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`product_category_id`);
+
+--
+-- Indexes for table `user_cart`
+--
+ALTER TABLE `user_cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `user_cart_cart_id` (`user_cart_cart_id`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`userinfo_id`);
+
+--
+-- Indexes for table `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  ADD UNIQUE KEY `user_email_UNIQUE` (`user_email`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_state`
+--
+ALTER TABLE `order_state`
+  ADD CONSTRAINT `order_state_ibfk_1` FOREIGN KEY (`order_state_id`) REFERENCES `user_cart` (`cart_id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`product_category_product_category_id`) REFERENCES `product_category` (`product_category_id`);
+
+--
+-- Constraints for table `user_cart`
+--
+ALTER TABLE `user_cart`
+  ADD CONSTRAINT `user_cart_ibfk_1` FOREIGN KEY (`user_cart_cart_id`) REFERENCES `user_info` (`userinfo_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
